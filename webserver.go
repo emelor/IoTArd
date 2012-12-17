d831c421c9f589e782933a24793c2a427e1343e3
Emelie <emelor@gmail.com>
2012-12-17 16:53:03 +0100

    added go server and arduino client code

---
+++ webserver.go
@@ -0,0 +1,18 @@
+package main
+import "fmt"
+import "net/http"
+import "io"
+
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "hello, world!\n")
+}
+
+func main (){
+	fmt.Println("hello")
+	err := http.ListenAndServe(":12345", http.HandlerFunc(HelloServer))
+	if err != nil {
+		panic(err)
+	}
+	
+	
+}
